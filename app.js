070f03a13d72b7c5b31d195e8d6c4d99cadeb861
Alexander <alex.silcorp@gmail.com>
2024-01-06 22:30:29 +0100

    fix: use media alternatives according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="delete-btn";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="delete-btn__img";
+    deleteButtonImg.alt="";
     deleteButton.appendChild(deleteButtonImg);
 
 
